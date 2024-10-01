0ce537660d7e9ea1d5ed66fcfcd49bd596a8d721
Dávid Laurovič <david.laurovic@gmail.com>
2024-10-01 15:38:41 +0300

    feat(app): regenerated openapi.yaml

--- system-info-service-1-java/src/main/java/fi/tuni/compse140/exercise1/api/endpoint/JaxRsActivator.java
+++ system-info-service-1-java/src/main/java/fi/tuni/compse140/exercise1/api/endpoint/JaxRsActivator.java
@@ -11,7 +11,7 @@ import jakarta.ws.rs.core.Application;
 
 @OpenAPIDefinition(
         info = @Info(title = "System Info Service 1 - Java",
-                description = "Service1 acts as an HTTP server accessible, responding to external client requests by collecting system information from both itself and Service2. It communicates with Service2 to retrieve its IP address, running processes, available disk space, and uptime, and returns the combined data to the client.",
+                description = "Service1 acts as an accessible HTTP server, responding to external client requests by collecting system information from both itself and Service2. It communicates with Service2 to retrieve its IP address, running processes, available disk space, and uptime, and returns the combined data to the client.",
                 version = "1.0",
                 contact = @Contact(name = "Dávid Laurovič", email = "david.laurovic@tuni.fi"),
                 license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html")
