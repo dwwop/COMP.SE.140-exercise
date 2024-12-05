5eea5d387cc064bbb451fddd25501ff6db1d1df7
Dávid Laurovič <david.laurovic@gmail.com>
2024-12-05 15:09:21 +0200

    test(app): Add e2e tests for put state

--- tests/e2e_tests.py
+++ tests/e2e_tests.py
@@ -20,9 +20,7 @@ def test_request():
     assert "diskSpace" in response_data[0]
     assert "lastBootTime" in response_data[0]
 
-
-BASE_URL = "http://" + os.getenv("BASE_HOST", "localhost") + ":8197"
-
+@pytest.mark.run(order=0)
 def test_get_state():
     response = requests.get(f"{BASE_URL}/state")
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
@@ -31,3 +29,28 @@ def test_get_state():
     response_data = response.text
     assert "INIT" == response_data, f"Unexpected response body: {response_data}"
 
+@pytest.mark.run(order=1)
+def test_put_state():
+    response = requests.put(f"{BASE_URL}/state", json={'state': 'RUNNING'})
+    assert response.status_code == 409, f"Unexpected status code: {response.status_code}"
+    try:
+        response_data = response.json()
+    except ValueError:
+        assert False, "Response is not a valid JSON"
+    
+    assert response_data["errorMessage"] == "The system cannot transition directly from INIT to RUNNING"
+
+    response = requests.post(f"{BASE_URL}/state/running")
+    assert response.status_code == 204, f"Unexpected status code: {response.status_code}"
+
+    response = requests.get(f"{BASE_URL}/state")
+    assert "RUNNING" == response_data, f"Unexpected response body: {response_data}"
+
+    response = requests.put(f"{BASE_URL}/state", json={'state': 'PAUSED'})
+    assert response.status_code == 204, f"Unexpected status code: {response.status_code}"
+
+    response = requests.get(f"{BASE_URL}/state")
+    assert "PAUSED" == response_data, f"Unexpected response body: {response_data}"
+    
+    response = requests.put(f"{BASE_URL}/state", json={'state': 'INVALID'})
+    assert response.status_code == 400, f"Unexpected status code: {response.status_code}"
