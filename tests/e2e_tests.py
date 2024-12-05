a131bc5b2a0adad7556d698444206d3bb5d4f55f
Dávid Laurovič <david.laurovic@gmail.com>
2024-12-05 19:36:08 +0200

    test(app): shutdown

--- tests/e2e_tests.py
+++ tests/e2e_tests.py
@@ -1,12 +1,14 @@
 import requests
 import os
 import pytest
+import time
 
-BASE_URL = "http://" + os.getenv("BASE_HOST", "localhost") + ":8197"
+API_GATEWAY_URL = "http://" + os.getenv("BASE_HOST", "localhost") + ":8197"
+BROWSER_URL =  "http://" + os.getenv("BASE_HOST", "localhost") + ":8198"
 
 @pytest.mark.run(order=0)
 def test_request():
-    response = requests.get(f"{BASE_URL}/request")
+    response = requests.get(f"{API_GATEWAY_URL}/request")
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
     try:
         response_data = response.json()
@@ -24,7 +26,7 @@ def test_request():
 
 @pytest.mark.run(order=1)
 def test_get_state():
-    response = requests.get(f"{BASE_URL}/state")
+    response = requests.get(f"{API_GATEWAY_URL}/state")
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
     assert 'text/plain' in response.headers['Content-Type'], "Expected Content-Type is text/plain"
 
@@ -33,7 +35,7 @@ def test_get_state():
 
 @pytest.mark.run(order=2)
 def test_put_state():
-    response = requests.put(f"{BASE_URL}/state", json={'state': 'RUNNING'})
+    response = requests.put(f"{API_GATEWAY_URL}/state", json={'state': 'RUNNING'})
     assert response.status_code == 409, f"Unexpected status code: {response.status_code}"
     try:
         response_data = response.json()
@@ -42,26 +44,26 @@ def test_put_state():
     
     assert response_data["errorMessage"] == "The system cannot transition directly from INIT to RUNNING"
 
-    response = requests.post(f"{BASE_URL}/state/running")
+    response = requests.post(f"{API_GATEWAY_URL}/state/running")
     assert response.status_code == 204, f"Unexpected status code: {response.status_code}"
 
-    response = requests.get(f"{BASE_URL}/state")
+    response = requests.get(f"{API_GATEWAY_URL}/state")
     response_data = response.text
     assert "RUNNING" == response_data, f"Unexpected response body: {response_data}"
 
-    response = requests.put(f"{BASE_URL}/state", json={'state': 'PAUSED'})
+    response = requests.put(f"{API_GATEWAY_URL}/state", json={'state': 'PAUSED'})
     assert response.status_code == 204, f"Unexpected status code: {response.status_code}"
 
-    response = requests.get(f"{BASE_URL}/state")
+    response = requests.get(f"{API_GATEWAY_URL}/state")
     response_data = response.text
     assert "PAUSED" == response_data, f"Unexpected response body: {response_data}"
     
-    response = requests.put(f"{BASE_URL}/state", json={'state': 'INVALID'})
+    response = requests.put(f"{API_GATEWAY_URL}/state", json={'state': 'INVALID'})
     assert response.status_code == 400, f"Unexpected status code: {response.status_code}"
 
 @pytest.mark.run(order=3)
 def test_get_log():
-    response = requests.get(f"{BASE_URL}/run-log")
+    response = requests.get(f"{API_GATEWAY_URL}/run-log")
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
     assert 'text/plain' in response.headers['Content-Type'], "Expected Content-Type is text/plain"
 
@@ -72,27 +74,46 @@ def test_get_log():
 
 @pytest.mark.run(order=4)
 def test_request_count():
-    response = requests.get(f"{BASE_URL}/request-count/api")
+    response = requests.get(f"{API_GATEWAY_URL}/request-count/api")
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
     assert 'text/plain' in response.headers['Content-Type'], "Expected Content-Type is text/plain"
 
     response_data = response.text
     assert "1" == response_data, f"Unexpected response body: {response_data}"
 
-    response = requests.get(f"{BASE_URL}/request-count/browser")
+    response = requests.get(f"{API_GATEWAY_URL}/request-count/browser")
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
     assert 'text/plain' in response.headers['Content-Type'], "Expected Content-Type is text/plain"
 
     response_data = response.text
     assert "0" == response_data, f"Unexpected response body: {response_data}"
 
-    response = requests.put(f"{BASE_URL}/request-count/browser")
+    response = requests.put(f"{API_GATEWAY_URL}/request-count/browser")
     assert response.status_code == 204, f"Unexpected status code: {response.status_code}"
     
-    response = requests.get(f"{BASE_URL}/request-count/browser")
+    response = requests.get(f"{API_GATEWAY_URL}/request-count/browser")
     response_data = response.text
     assert "1" == response_data, f"Unexpected response body: {response_data}"
     
-    response = requests.get(f"{BASE_URL}/request-count/api")
+    response = requests.get(f"{API_GATEWAY_URL}/request-count/api")
     response_data = response.text
     assert "1" == response_data, f"Unexpected response body: {response_data}"
+
+@pytest.mark.run(order=5)
+def test_shutdown_service():
+    response = requests.put(f"{BROWSER_URL}/state", json={'state': 'SHUTDOWN'})
+    assert response.status_code == 204, f"Unexpected status code: {response.status_code}"
+
+    time.sleep(3)
+    
+    try:
+        result = subprocess.run(
+            ['docker', 'ps', '--filter', 'name=compse140-exercise-', '--format', '{{.Names}}'],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+        
+        running_containers = result.stdout.strip().splitlines()
+        assert not running_containers, f"Containers are still running: {running_containers}"
+    
+    except subprocess.CalledProcessError as e:
+        assert False, f"Error checking running containers: {e}"
