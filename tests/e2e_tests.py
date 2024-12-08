e147da66403fdca4ae0f582b456e39cd96eff02e
Dávid Laurovič <david.laurovic@gmail.com>
2024-12-08 21:31:45 +0200

    test(app): make request respond with text plain

--- tests/e2e_tests.py
+++ tests/e2e_tests.py
@@ -12,19 +12,12 @@ def test_request():
     response = requests.get(f"{API_GATEWAY_URL}/request")
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
     assert 'text/plain' in response.headers['Content-Type'], "Expected Content-Type is text/plain"
-    try:
-        response_data = response.json()
-    except ValueError:
-        assert False, "Response is not a valid JSON"
-    
-    assert isinstance(response_data, list), f"Expected a list, got {type(response_data).__name__}"
-    
-    assert len(response_data) == 2, f"Expected array length 2, got {len(response_data)}"
+    response_data = response.text()
 
-    assert "ipAddress" in response_data[0]
-    assert "processes" in response_data[0]
-    assert "diskSpace" in response_data[0]
-    assert "lastBootTime" in response_data[0]
+    assert "ipAddress" in response_data
+    assert "processes" in response_data
+    assert "diskSpace" in response_data
+    assert "lastBootTime" in response_data
 
 @pytest.mark.run(order=1)
 def test_get_state():
