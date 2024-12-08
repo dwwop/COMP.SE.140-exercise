2e4f49fd9e909413b06d3fe249c4564b5bea0229
Dávid Laurovič <david.laurovic@gmail.com>
2024-12-08 17:22:32 +0200

    Revert "feat(app): auth cookie"
    This reverts commit 1437da813852eb745b0cdcd15f552e5feedf304f.

--- state-service/src/main/java/fi/tuni/compse140/project/api/endpoint/AuthControllerImpl.java
+++ state-service/src/main/java/fi/tuni/compse140/project/api/endpoint/AuthControllerImpl.java
@@ -4,7 +4,6 @@ import fi.tuni.compse140.project.api.dto.AuthDTO;
 import fi.tuni.compse140.project.api.dto.AuthRespDTO;
 import fi.tuni.compse140.project.facade.AuthFacade;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +18,6 @@ public class AuthControllerImpl implements AuthController {
 
     @Override
     public ResponseEntity<AuthRespDTO> verify(AuthDTO authDTO) {
-        AuthRespDTO authRespDTO = facade.verify(authDTO);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.SET_COOKIE, "authenticated=" + authRespDTO.authenticated() + "; HttpOnly; Path=/");
-
-        return ResponseEntity.ok()
-                .headers(headers)
-                .body(authRespDTO);
+        return ResponseEntity.ok(facade.verify(authDTO));
     }
 }
